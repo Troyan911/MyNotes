e6383116c8e95b03c27fafa761dd447aee0f8578
troyan911 <troyan911@gmail.com>
2023-12-16 20:50:23 +0200

    renamed folders

--- config/configurations.php
+++ config/configurations.php
@@ -6,5 +6,10 @@ return [
         'database' => getenv('DB_NAME') ?? null,
         'user' => getenv('DB_USER') ?? null,
         'password' => getenv('DB_PASS') ?? null
+    ],
+    'qwe' => [
+        'rty'=> [
+            'asd' => 'test'
+        ]
     ]
 ];
