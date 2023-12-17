87aa5618a7360e301c5e0882661cd2f8ffc23db0
troyan911 <troyan911@gmail.com>
2023-12-17 04:36:01 +0200

    refactoring

--- public/index.php
+++ public/index.php
@@ -1,19 +1,12 @@
 <?php
 
-define('BASE_DIR', dirname(__DIR__));
-require_once BASE_DIR . "/config/constants.php";
-require_once BASE_DIR . "/vendor/autoload.php";
+require_once dirname(__DIR__) . "/core/SourceLoader.php";
 
 //dd(TEST);
 
 use Core\Router;
 
 try {
-    $dotenv = \Dotenv\Dotenv::createUnsafeImmutable(BASE_DIR);
-//    $dotenv->required(['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASS']);
-    $dotenv->load();
-
-    dd(\Core\Config::get("db.user"));
 
     if(!preg_match("/assets/i", $_SERVER['REQUEST_URI'])) {
         Router::dispatch($_SERVER['REQUEST_URI']);
